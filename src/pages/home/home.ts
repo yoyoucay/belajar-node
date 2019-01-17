c2acfc8b319542a97ab3824c81ff0fc05f0c160f
LaboratoriumIF <admin@LabAnim-11x.local>
2019-01-17 20:30:54 +0700

    Belajar Node.js ada yang salah hihi

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DetailPage } from '../detail/detail';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  data: any = {
+    nama: "", pendapat: ""
+  };
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  submit(){
+    console.log(this.data);
+    this.navCtrl.push(DetailPage, this.data);
+  }
 }
